728f53668e21d479cf80acf45309be1b8542140d
Th3Catalyst <max675295@gmail.com>
2026-05-25 21:34:50 -0700

    slightly better title screen | cleaned up pegs script

--- scripts/createPegs.gd
+++ scripts/createPegs.gd
@@ -1,20 +1,35 @@
+
 extends Node2D
 
 var peg_scene: PackedScene = preload("res://scenes/peg.tscn")
 var ball_scene: PackedScene = preload("res://scenes/ball.tscn")
 
+var height: int = 1500
+var rows: int = 15
+var center: int = 2900
+var top: int = 400
+var spacing_v: int = height/rows
+var spacing_h: int = spacing_v/sqrt(0.75)
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready() -> void:
-	for i in range(5):
+	for i in range(rows):
 		for j in range(3+i):
 			var peg: Node2D = peg_scene.instantiate()
 			add_child(peg)
-			peg.position = Vector2(700-50*i+100*j, 70+100*i)
+			for child in peg.get_child(0).get_children():
+				child.scale *= spacing_h/100
+			peg.position = Vector2(center-(spacing_h/2)*i+spacing_h*(j-1), top+spacing_v*i)
+
 
 func _input(ev):
 	if Input.is_key_pressed(KEY_SPACE):
+		$SPACE_text.visible = false
 		var ball: Node2D = ball_scene.instantiate()
 		add_child(ball)
-		ball.position = Vector2(800+randf_range(-100,100), 0)
+		
+		ball.position = Vector2(center+randf_range(-spacing_h*0.7,spacing_h*0.7), 0)
+		for child in ball.get_child(0).get_children():
+			child.scale *= spacing_h/100
 		
