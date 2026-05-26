d772896a8c51e67982fd71a87a0a6d1e7f7496b9
5Stygian <rlcrafistohard@gmail.com>
2026-05-26 09:39:15 -0700

    try to follow bamon level best practice but fail miserably

--- scripts/createPegs.gd
+++ scripts/createPegs.gd
@@ -1,30 +1,18 @@
 extends Node2D
 
-var height: int = 1500
-var rows: int = 15
-var center: int = 2900
-var top: int = 400
-var spacing_v: float = height/rows
-var spacing_h: float = spacing_v/sqrt(0.75)
-
-
 func _ready() -> void:
-	Game.pegs_center = center
-	
-	for i in range(rows):
+	for i in range(Game.rows):
 		for j in range(3+i):
 			var peg: Node2D = Game.peg_scene.instantiate()
 			add_child(peg)
 			for child in peg.get_child(0).get_children():
-				child.scale *= spacing_h/100
-			peg.position = Vector2(center-(spacing_h/2)*i+spacing_h*(j-1), top+spacing_v*i)
+				child.scale *= Game.spacing_h/100
+			peg.position = Vector2(Game.center-(Game.spacing_h/2)*i+Game.spacing_h*(j-1), Game.top+Game.spacing_v*i)
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
+	# i dont like putting this here because this script is
+	# for creating the pegs and this does something completely different
+	# but putting this in game.gd makes it not work
+	# and i have no idea why
 	if Input.is_key_pressed(KEY_SPACE):
 		$SPACE_text.visible = false
-		var ball: Node2D = Game.ball_scene.instantiate()
-		add_child(ball)
-		ball.position = Vector2(center+randf_range(-spacing_h*0.7,spacing_h*0.7), 0)
-		for child in ball.get_child(0).get_children():
-			child.scale *= spacing_h/100
-		
--- scripts/game.gd
+++ scripts/game.gd
@@ -3,8 +3,14 @@ extends Node
 var peg_scene: PackedScene = preload("res://scenes/peg.tscn")
 var ball_scene: PackedScene = preload("res://scenes/ball.tscn")
 
-var pegs_center: float
 var money: float
+
+var height: float = 1500.0
+var rows: float = 15.0
+var center: float = 2900.0
+var top: float = 400.0
+var spacing_v: float = height/rows
+var spacing_h: float = spacing_v/sqrt(0.75)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,5 +18,10 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if Input.is_key_pressed(KEY_SPACE):
+		var ball: Node2D = Game.ball_scene.instantiate()
+		add_child(ball)
+		ball.position = Vector2(center+randf_range(-spacing_h*0.7,spacing_h*0.7), 0)
+		for child in ball.get_child(0).get_children():
+			child.scale *= spacing_h/100
