383242d623181aca227deaacf07ddd733c06cc16
Th3Catalyst <max675295@gmail.com>
2026-06-02 15:44:04 -0700

    new art

--- dropBall.gd
+++ dropBall.gd
@@ -4,7 +4,9 @@ func _input(_event: InputEvent) -> void:
 	var bet: float = round(float($".".text)*100)/100
 	if Input.is_key_pressed(KEY_SPACE) and Game.money >= bet:
 		var ball: Node2D = Game.ball_scene.instantiate()
-		ball.set_meta("Value", bet) 
+		ball.set_meta("Value", bet)
+		#ball.get_child(0).get_child(1).material.set_shader_parameter("saturation", randf() * 100)
+		#ball.set_instance_shader_parameter("hue_rotate", 180.0)
 		Game.money -= bet
 		$".".get_parent().add_child(ball)
 		ball.position = Vector2(randfn(Game.center, Game.spacing_h*0.3), Game.top-70)
