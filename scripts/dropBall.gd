3e6b0903dcc888729225098c8c3dcc46fd42b406
Th3Catalyst <max675295@gmail.com>
2026-06-08 19:18:46 -0700

    resolve merge conflicts

--- scripts/dropBall.gd
+++ scripts/dropBall.gd
@@ -7,7 +7,14 @@ func drop_ball() -> void:
 	var bet: float = round(float($".".text)*100)/100
 	if Game.money >= bet:
 		var ball: Node2D = Game.ball_scene.instantiate()
-		ball.set_meta("Value", bet)
+		var ball_value_mod: float = Game.ball_costs[floor(randfn(1.8, 0.6))]
+		var ball_value: float = bet + ball_value_mod/2
+		
+		print("ball_value_mod: ", ball_value_mod)
+		print("bet: ", bet)
+		print("ball_value: ", ball_value)
+		
+		ball.set_meta("Value", ball_value)
 		Game.money -= bet
 		$".".get_parent().add_child(ball)
 		ball.position = Vector2(randfn(Game.center, Game.spacing_h*0.3), Game.top-70) 
--- scripts/gainMoney.gd
+++ scripts/gainMoney.gd
@@ -6,6 +6,8 @@ func _ready():
 func _on_body_entered(body: RigidBody2D) -> void:
 	if body.name == "BallBody":
 		var ball: Node2D = body.get_parent()
-		Game.money += ball.get_meta("Value") * $".".get_parent().get_meta("multiplier")
+		var money: float = ball.get_meta("Value") * $".".get_parent().get_meta("multiplier")
+		print("money rewarded: ", money)
+		Game.money += money
 		$CollectSound.play()
 		ball.queue_free()
